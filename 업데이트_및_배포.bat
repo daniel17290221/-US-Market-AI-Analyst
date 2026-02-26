e0fc80ce466b0d5f56ad5ab400dd00ce7f232cd5
daniel <1729021@naver.com>
2026-02-26 09:51:14 +0800

    aa

--- 업데이트_및_배포.bat
+++ 업데이트_및_배포.bat
@@ -1,21 +1,14 @@
 @echo off
-echo [1/3] Master 브랜치 최신화 및 커밋 중...
+echo [1/2] Main 브랜치 최신화 및 커밋 중...
 git add .
 git commit -m "Manual sync and deploy: %date% %time%"
 
 echo.
-echo [2/3] Master 내용을 Main 브랜치로 강제 병합 중...
-git checkout main
-git reset --hard master
-
-echo.
-echo [3/3] GitHub로 푸시 중 (Vercel 배포 트리거)...
-git push origin master --force
-git push origin main --force
+echo [2/2] GitHub Main 브랜치로 푸시 중 (Vercel 배포 트리거)...
+git push origin main
 
 echo.
 echo [!] 모든 작업 완료! 
-echo [!] 다시 Master 브랜치로 돌아갑니다.
-git checkout master
+echo [!] Vercel 대시보드에서 빌드 상태를 확인하세요.
 
 pause
