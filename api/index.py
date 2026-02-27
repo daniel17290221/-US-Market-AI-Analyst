d47d6a8162aa0e3c1b2e8fbe0ec496e522bb73f1
daniel <1729021@naver.com>
2026-02-27 08:17:37 +0800

    Fix NameError: define app before after_request decorator

--- api/index.py
+++ api/index.py
@@ -3,13 +3,7 @@ from typing import Dict
 import csv
 import json
 
-# CORS 허용 (Virtual Protocol 대시보드 연동용)
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    return response
+
 
 # --- Config ---
 # --- Omni Swarm v3.5: Multi-Agent Intelligent Analytics ---
